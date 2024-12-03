3abb9f2b34286e11853455aaf6207c0cf9ba85db
Arián Jiménez Majidi <arianjm@gmail.com>
2024-12-03 23:07:56 +0100

    Add day 2 part 2

--- 24/02/main.rb
+++ 24/02/main.rb
@@ -1,11 +1,23 @@
 file = File.open('input.txt')
 
+def check_conditions(levels)
+  (levels == levels.sort || levels == levels.sort.reverse) && levels.each_cons(2).all? { |a, b| (a - b).abs.between?(1, 3) }
+end
+
 reports = file.readlines.map { |line| line.split.map(&:to_i) }
 num_safe_reports = 0
 reports.each do |report|
-  num_safe_reports += 1 if (report == report.sort ||
-                            report == report.sort.reverse) &&
-                           report.each_cons(2).all? { |a, b| (a - b).abs.between?(1, 3) }
+  num_safe_reports += 1 if check_conditions(report)
 end
 
-puts num_safe_reports
+puts "Part 1 answer: #{num_safe_reports}"
+
+num_safe_reports = 0
+reports.each do |report|
+  (0...report.length).any? do |index|
+    levels_list = report[0...index] + report[index+1..-1]
+    num_safe_reports += 1 if check_conditions(levels_list)
+  end
+end
+
+puts "Part 2 answer: #{num_safe_reports}"
