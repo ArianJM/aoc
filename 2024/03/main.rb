8c6222ca5b9bd5a326c104306eb61e46ec790fcc
Arián Jiménez Majidi <arianjm@gmail.com>
2024-12-04 22:14:31 +0100

    Day 3 parts 1 and 2

---
+++ 2024/03/main.rb
@@ -0,0 +1,12 @@
+content = File.read('input.txt')
+
+def find_muls_and_multiply(string)
+  matches = string.scan(/mul\((\d{1,3}),(\d{1,3})\)/)
+  matches.sum { (_1.to_i * _2.to_i) }
+end
+
+puts "First part answer: #{find_muls_and_multiply(content)}"
+
+content_for_part_2 = content.gsub(/don't\(\).*?do\(\)/m, '')
+
+puts "Second part answer: #{find_muls_and_multiply(content_for_part_2)}"
