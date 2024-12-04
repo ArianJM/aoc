1b462e465743f088be9a0b54eae9caab6987474c
Arián Jiménez Majidi <arianjm@gmail.com>
2024-12-04 17:41:05 +0100

    Change to use numbered parameters

--- 2024/01/main.rb
+++ 2024/01/main.rb
@@ -1,14 +1,14 @@
 file = File.open('input.txt')
 
-columns = file.readlines.map { |line| line.split.map(&:to_i) }.transpose
+columns = file.readlines.map { _1.split.map(&:to_i) }.transpose
 sorted_columns = columns.map(&:sort)
 
-difference_sum = sorted_columns[0].zip(sorted_columns[1]).sum { |a, b| (a - b).abs }
+difference_sum = sorted_columns[0].zip(sorted_columns[1]).sum { (_1 - _2).abs }
 
 puts "First part answer: #{difference_sum}"
 
 count_second = sorted_columns[1].tally
 
-similarity_score = sorted_columns[0].sum { |num| num * (count_second[num] || 0) }
+similarity_score = sorted_columns[0].sum { _1 * (count_second[_1] || 0) }
 
 puts "Second part answer: #{similarity_score}"
--- 2024/02/main.rb
+++ 2024/02/main.rb
@@ -1,13 +1,13 @@
 file = File.open('input.txt')
 
 def check_conditions(levels)
-  (levels == levels.sort || levels == levels.sort.reverse) && levels.each_cons(2).all? { |a, b| (a - b).abs.between?(1, 3) }
+  (levels == levels.sort || levels == levels.sort.reverse) && levels.each_cons(2).all? { (_1 - _2).abs.between?(1, 3) }
 end
 
-reports = file.readlines.map { |line| line.split.map(&:to_i) }
+reports = file.readlines.map { _1.split.map(&:to_i) }
 num_safe_reports = 0
-reports.each do |report|
-  num_safe_reports += 1 if check_conditions(report)
+reports.each do
+  num_safe_reports += 1 if check_conditions(_1)
 end
 
 puts "Part 1 answer: #{num_safe_reports}"
